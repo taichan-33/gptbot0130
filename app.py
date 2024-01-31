3061b341673182f9cce557d3ab05b87e0a5978e3
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-01-31 19:23:35 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_chat import message
 import openai
 
 # OpenAI APIキーの設定
@@ -8,10 +9,6 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 st.title("QUICKFIT BOT")
 st.write("Quick fitに関するQ&A AIBOT")
 
-# デフォルトモデルの設定
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4-0125-preview"
-
 # メッセージ履歴の初期化と初期プロンプトの設定
 if "messages" not in st.session_state:
     initial_prompt = str(st.secrets.AppSettings.initial_prompt)
@@ -19,32 +16,30 @@ if "messages" not in st.session_state:
         {"role": "system", "content": initial_prompt}
     ]
 
-try:
-    # ユーザーの入力を取得
-    prompt = st.chat_input("Your message here:")
-    if prompt:
-        # ユーザーメッセージを履歴に追加
-        st.session_state.messages.append({"role": "user", "content": prompt})
+# ユーザーの入力を取得
+with st.container():
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_input("Your message here:", placeholder="Ask your question here", key='input')
+        submit_button = st.form_submit_button(label='Send')
 
-        # ユーザーメッセージを表示
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    if submit_button and user_input:
+        # ユーザーメッセージを履歴に追加
+        st.session_state["messages"].append({"role": "user", "content": user_input})
 
         # OpenAIから応答を取得
         response = openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-            stream=True
+            model="gpt-4-0125-preview",
+            messages=st.session_state["messages"]
         )
 
-        # ボットの応答を表示
-        full_response = ""
-        for part in response:
-            full_response += part['choices'][0]['delta'].get('content', '')
-        with st.chat_message("assistant"):
-            st.markdown(full_response)
+        # ボットの応答を履歴に追加
+        full_response = response["choices"][0]["message"]["content"]
+        st.session_state["messages"].append({"role": "assistant", "content": full_response})
 
-        # 応答を履歴に追加
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-except Exception as e:
-    st.error(f"エラーが発生しました: {e}")
+# ユーザーとチャットボットのメッセージ表示
+with st.container():
+    for message_info in st.session_state["messages"]:
+        if message_info["role"] == "user":
+            message(message_info["content"], is_user=True, avatar_style="big-smile")
+        elif message_info["role"] == "assistant":
+            message(message_info["content"], avatar_style="thumbs")
