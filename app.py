269546c391b588990def27a4b306d96de5262866
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-01-31 19:08:17 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 st.title("QUICKFIT BOT")
 st.write("Quick fitに関するQ&A AIBOT")
 
-
 # デフォルトモデルの設定
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4-0125-preview"
@@ -20,29 +19,32 @@ if "messages" not in st.session_state:
         {"role": "system", "content": initial_prompt}
     ]
 
-# ユーザーの入力を取得
-prompt = st.chat_input("Your message here:")
-if prompt:
-    # ユーザーメッセージを履歴に追加
-    st.session_state.messages.append({"role": "user", "content": prompt})
+try:
+    # ユーザーの入力を取得
+    prompt = st.chat_input("Your message here:")
+    if prompt:
+        # ユーザーメッセージを履歴に追加
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # ユーザーメッセージを表示
-    with st.chat_message("user"):
-        st.markdown(prompt)
+        # ユーザーメッセージを表示
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # OpenAIから応答を取得
-    response = openai.ChatCompletion.create(
-        model=st.session_state["openai_model"],
-        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-        stream=True
-    )
+        # OpenAIから応答を取得
+        response = openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True
+        )
 
-    # ボットの応答を表示
-    full_response = ""
-    for part in response:
-        full_response += part['choices'][0]['delta'].get('content', '')
-    with st.chat_message("assistant"):
-        st.markdown(full_response)
+        # ボットの応答を表示
+        full_response = ""
+        for part in response:
+            full_response += part['choices'][0]['delta'].get('content', '')
+        with st.chat_message("assistant"):
+            st.markdown(full_response)
 
-    # 応答を履歴に追加
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        # 応答を履歴に追加
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+except Exception as e:
+    st.error(f"エラーが発生しました: {e}")
