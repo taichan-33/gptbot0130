1ab2e8e3a1b950ced01d53845b166c70933b8e80
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-02-01 10:33:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -33,27 +33,7 @@ HIDE_ST_STYLE = """
                 visibility: hidden;
                 height: 0%;
                 }
-				        .appview-container .main .block-container{
-                            padding-top: 1rem;
-                            padding-right: 3rem;
-                            padding-left: 3rem;
-                            padding-bottom: 1rem;
-                        }  
-                        .reportview-container {
-                            padding-top: 0rem;
-                            padding-right: 3rem;
-                            padding-left: 3rem;
-                            padding-bottom: 0rem;
-                        }
-                        header[data-testid="stHeader"] {
-                            z-index: -1;
-                        }
-                        div[data-testid="stToolbar"] {
-                        z-index: 100;
-                        }
-                        div[data-testid="stDecoration"] {
-                        z-index: 100;
-                        }
+				        
                 </style>
 """
 
