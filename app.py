1049e6feb0f80331ee1007462d9389730e24ac8a
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-03-06 15:49:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 import os
 import openai
 import streamlit as st
+from anthropic import Anthropic
 
 # OpenAI APIキーの設定
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
 
+# Anthropic APIキーの設定
+anthropic_api_key = st.secrets["AnthropicAPI"]["anthropic_api_key"]
 
 st.title("QUICKFIT BOT")
 st.write("Quick fitに関するQ&A AIBOT")
 
 HIDE_ST_STYLE = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-				        
-                </style>
+<style>
+div[data-testid="stToolbar"] {
+visibility: hidden;
+height: 0%;
+position: fixed;
+}
+div[data-testid="stDecoration"] {
+visibility: hidden;
+height: 0%;
+position: fixed;
+}
+</style>
 """
-
 st.markdown(HIDE_ST_STYLE, unsafe_allow_html=True)
-
 
 # 定数定義
 USER_NAME = "user"
@@ -37,56 +37,94 @@ client = openai.ChatCompletion()
 
 def response_chatgpt(user_msg: str, past_messages: list):
     """ChatGPTのレスポンスを取得
-
+    
     Args:
         user_msg (str): ユーザーメッセージ。
         past_messages (list): 過去のメッセージリスト（ユーザーとアシスタントの両方）。
     """
     # 過去のメッセージに現在のメッセージを追加
     messages_to_send = past_messages + [{"role": "user", "content": user_msg}]
-
+    
     # ChatGPTにメッセージを送信し、レスポンスを取得
     response = client.create(
         model="gpt-4-0125-preview",
         messages=messages_to_send,
         stream=True,
     )
+    
     return response
+
+def response_claude(user_msg: str, past_messages: list):
+    """Claude-3のレスポンスを取得
+    
+    Args:
+        user_msg (str): ユーザーメッセージ。
+        past_messages (list): 過去のメッセージリスト（ユーザーとアシスタントの両方）。
+    """
+    anthropic = Anthropic(anthropic_api_key)
+    
+    # 過去のメッセージに現在のメッセージを追加 
+    messages_to_send = past_messages + [{"role": "user", "content": user_msg}]
+    
+    # Claude-3にメッセージを送信し、レスポンスを取得
+    response = anthropic.messages.create(
+        model="claude-3-opus-20240229",
+        messages=messages_to_send,
+        max_tokens=2000,
+        stream=True,
+    )
+    
+    return response
+
+# サイドバーの追加
+model = st.sidebar.selectbox("Select Model", ["chatgpt", "claude3 opus"])
 
 # メッセージ履歴の初期化と初期プロンプトの設定
 if "messages" not in st.session_state:
     initial_prompt = str(st.secrets["AppSettings"]["initial_prompt"])
     st.session_state["messages"] = [{"role": "system", "content": initial_prompt}]
 
-# チャットログを保存したセッション情報を初期化
+# チャットログを保存したセッション情報を初期化 
 if "chat_log" not in st.session_state:
     st.session_state.chat_log = []
 
 user_msg = st.chat_input("ここにメッセージを入力")
+
 if user_msg:
     # 以前のチャットログを表示
     for chat in st.session_state.chat_log:
         with st.chat_message(chat["name"]):
             st.write(chat["msg"])
-
+            
     # 最新のメッセージを表示
     with st.chat_message(USER_NAME):
         st.write(user_msg)
-
-    # アシスタントのメッセージを表示
-    response = response_chatgpt(user_msg, st.session_state["messages"])
+        
+    # モデルに応じてレスポンスを取得
+    if model == "chatgpt":
+        response = response_chatgpt(user_msg, st.session_state["messages"])
+    elif model == "claude3 opus":
+        response = response_claude(user_msg, st.session_state["messages"])
+        
+    # アシスタントのメッセージを表示  
     with st.chat_message(ASSISTANT_NAME):
         assistant_msg = ""
         assistant_response_area = st.empty()
         for chunk in response:
-            if chunk.choices[0].finish_reason is not None:
-                break
-            # 回答を逐次表示
-            assistant_msg += chunk.choices[0].delta.content
+            if isinstance(chunk, dict):
+                if chunk.get("finish_reason") is not None:
+                    break
+                # Claude-3の回答を逐次表示
+                assistant_msg += chunk["content"]
+            else:
+                # ChatGPTの回答を逐次表示
+                if chunk.choices[0].finish_reason is not None:
+                    break
+                assistant_msg += chunk.choices[0].delta.content
             assistant_response_area.write(assistant_msg)
-
+            
     # セッションにチャットログを追加
     st.session_state["messages"].append({"role": "user", "content": user_msg})
     st.session_state["messages"].append({"role": "assistant", "content": assistant_msg})
-    st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg})
+    st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg}) 
     st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": assistant_msg})
