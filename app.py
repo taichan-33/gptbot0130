545ab139b2657b2e8e047fd8631c6698a524508f
taichan-33 <151983276+taichan-33@users.noreply.github.com>
2024-03-06 19:00:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import openai
 import streamlit as st
 from anthropic import Anthropic
+from anthropic.events import MessageStartEvent, MessageDoneEvent, MessageContentEvent
 
 # OpenAI APIキーの設定
 openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
